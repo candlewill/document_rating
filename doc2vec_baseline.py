3219ae5397c3a6dd21466df77a712cdad6318165
HYC <yunchaohe@gmail.com>
2015-08-06 20:35:08 +0800

    doc2vec works well

--- doc2vec_baseline.py
+++ doc2vec_baseline.py
@@ -51,17 +51,27 @@ keys = list(sources.keys())
 for old_key in keys:
     sources[dir + '/' + old_key] = sources.pop(old_key)
 ##############################################################
+############################# model training #################
+'''
 sentences = LabeledLineSentence(sources)
 model = Doc2Vec(min_count=1, window=10, size=100, sample=1e-4, negative=5, workers=7)
 model.build_vocab(sentences.to_array())
-for epoch in range(1):
+for epoch in range(10):
+    print('epoch: %s' % epoch)
     model.train(sentences.sentences_rand())
 
 print(model.most_similar('good'))
-print(model['TRAIN_NEG_0'])
+# print(model['TRAIN_NEG_0'])
 
 model.save(get_file_path('test_doc2vec_model'))
-# model = Doc2Vec.load('./imdb.d2v')
+print('model been saved!')
+'''
+############################# model training #################
+
+model = Doc2Vec.load(get_file_path('test_doc2vec_model'))
+print(model.most_similar('good'))
+print(model.docvecs[1])
+print(model.docvecs['TRAIN_NEG_1'])
 
 train_arrays = numpy.zeros((25000, 100))
 train_labels = numpy.zeros(25000)
@@ -69,8 +79,8 @@ train_labels = numpy.zeros(25000)
 for i in range(12500):
     prefix_train_pos = 'TRAIN_POS_' + str(i)
     prefix_train_neg = 'TRAIN_NEG_' + str(i)
-    train_arrays[i] = model[prefix_train_pos]
-    train_arrays[12500 + i] = model[prefix_train_neg]
+    train_arrays[i] = model.docvecs[prefix_train_pos]
+    train_arrays[12500 + i] = model.docvecs[prefix_train_neg]
     train_labels[i] = 1
     train_labels[12500 + i] = 0
 
@@ -83,8 +93,8 @@ test_labels = numpy.zeros(25000)
 for i in range(12500):
     prefix_test_pos = 'TEST_POS_' + str(i)
     prefix_test_neg = 'TEST_NEG_' + str(i)
-    test_arrays[i] = model[prefix_test_pos]
-    test_arrays[12500 + i] = model[prefix_test_neg]
+    test_arrays[i] = model.docvecs[prefix_test_pos]
+    test_arrays[12500 + i] = model.docvecs[prefix_test_neg]
     test_labels[i] = 1
     test_labels[12500 + i] = 0
 
