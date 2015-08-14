3586125906c540c279c318d1afc4e121359b00f5
nobody <yunchaohe@gmail.com>
2015-08-15 00:32:26 +0800

    add more examples for cnn

--- examples/imdb_cnn.py
+++ examples/imdb_cnn.py
@@ -23,13 +23,13 @@ from keras.datasets import imdb
 
 # set parameters:
 max_features = 5000
-maxlen = 100
+maxlen = 150
 batch_size = 32
 embedding_dims = 100
 nb_filters = 250
 filter_length = 3
 hidden_dims = 250
-nb_epoch = 3
+nb_epoch = 10
 
 print("Loading data...")
 (X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=max_features,
