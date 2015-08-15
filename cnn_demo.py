ab3e8eecb93b8a5ffaba1100e46c48910a363525
candlewill <yunchaohe@gmail.com>
2015-08-16 00:47:36 +0800

    optimise CVAT CNN demo

--- cnn_demo.py
+++ cnn_demo.py
@@ -1,16 +1,21 @@
 __author__ = 'NLP-PC'
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from keras.optimizers import SGD
+
 from load_data import load_embeddings, load_mark
+from load_data import load_corpus
+from file_name import get_file_path
+from word2vec_fn import build_embedding_matrix, build_sentence_matrix, gold_valence_arousal
+from sklearn import cross_validation
 
 word_vecs = load_embeddings('CVAT')
-
-from word2vec_fn import build_embedding_matrix, build_sentence_matrix, gold_valence_arousal
 
 embedding_matrix, idx_map = build_embedding_matrix(word_vecs, k=50)
 print(embedding_matrix[:1])
 print(idx_map['我們'])
-
-from load_data import load_corpus
-from file_name import get_file_path
 
 corpus = load_corpus(get_file_path('cn_corpus'))
 print(corpus[:2])
@@ -24,8 +29,6 @@ sentence_embedding_matrix = build_sentence_matrix(word_vecs, corpus)
 print(sentence_embedding_matrix.shape)
 print(sentence_embedding_matrix[3], valence[3], arousal[3])
 
-from sklearn import cross_validation
-
 X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(sentence_embedding_matrix, valence, test_size=0.2,
                                                                      random_state=0)
 print(X_train.shape)
@@ -35,12 +38,6 @@ size = 50
 X_train = X_train.reshape(X_train.shape[0], 1, maxlen, size)
 X_test = X_test.reshape(X_test.shape[0], 1, maxlen, size)
 print(X_train.shape)
-
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.utils import np_utils
-from keras.optimizers import SGD
 
 batch_size = 128
 nb_epoch = 12
@@ -67,3 +64,7 @@ sgd = SGD(lr=0.05, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='mean_squared_error', optimizer=sgd)
 
 model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(X_test, Y_test))
+
+score = model.evaluate(X_test, Y_test)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
