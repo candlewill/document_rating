35661773f4e234b471fab33e32a975bfc5b8905a
candlewill <yunchaohe@gmail.com>
2015-08-16 15:50:23 +0800

    optimise CVAT CNN demo

--- cnn_demo.py
+++ cnn_demo.py
@@ -1,15 +1,18 @@
-__author__ = 'NLP-PC'
+from __future__ import absolute_import
+from __future__ import print_function
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.optimizers import SGD
 
+from sklearn import cross_validation
+
 from load_data import load_embeddings, load_mark
 from load_data import load_corpus
 from file_name import get_file_path
 from word2vec_fn import build_embedding_matrix, build_sentence_matrix, gold_valence_arousal
-from sklearn import cross_validation
+
 
 word_vecs = load_embeddings('CVAT')
 
@@ -32,6 +35,7 @@ print(sentence_embedding_matrix[3], valence[3], arousal[3])
 X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(sentence_embedding_matrix, valence, test_size=0.2,
                                                                      random_state=0)
 print(X_train.shape)
+print(len(Y_test))
 
 maxlen = 200
 size = 50
@@ -40,7 +44,7 @@ X_test = X_test.reshape(X_test.shape[0], 1, maxlen, size)
 print(X_train.shape)
 
 batch_size = 128
-nb_epoch = 12
+nb_epoch = 1
 
 model = Sequential()
 
@@ -63,8 +67,14 @@ sgd = SGD(lr=0.05, decay=1e-6, momentum=0.9, nesterov=True)
 # model.compile(loss='mean_squared_error', optimizer='adagrad')
 model.compile(loss='mean_squared_error', optimizer=sgd)
 
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(X_test, Y_test))
+model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, validation_data=(X_test, Y_test))
 
 score = model.evaluate(X_test, Y_test)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+print('The score:', score)
+predict = model.predict(X_test, batch_size=batch_size).reshape((1, len(Y_test)))[0]
+
+from evaluate import evaluate
+
+print(Y_test)
+print(predict)
+evaluate(Y_test, predict, 'Result of CNN')
