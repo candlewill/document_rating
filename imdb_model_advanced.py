a804910e7164f7c207ea7ad22985a3dc27f03aba
candlewill <yunchaohe@gmail.com>
2015-08-18 17:33:51 +0800

    cnn for imdb using word2vec

--- imdb_model_advanced.py
+++ imdb_model_advanced.py
@@ -1,26 +1,38 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Reshape
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.optimizers import SGD
+from keras.constraints import unitnorm, maxnorm
+from keras.layers.embeddings import Embedding
+from keras.regularizers import l2
 
 import numpy as np
 import os
 import pickle
 from sklearn import cross_validation
+from sklearn.metrics import roc_auc_score
 
 ########################################## config ########################################
 vec_dim = 300
 max_len = 200
-kernel_size = 5
+kernel_size = 8
 filename = './data/tmp/imdb_processed_data.p'
+embedding_maxtrix = 'D:/DATA/embedding_matrix.p'
 ##########################################################################################
-database, pos_length, neg_length = pickle.load(open(filename, "rb"))
+dataset, pos_length, neg_length = pickle.load(open(filename, "rb"))
+W = pickle.load(open(embedding_maxtrix, "rb"))
+print(W.shape)
+print(dataset.shape)
+conv_input_width = W.shape[1]  # embedding dimension
+conv_input_height = int(dataset.shape[1])  # max_len
+print(conv_input_width, conv_input_height)
+
 Y = np.concatenate((np.ones((1, pos_length)), np.zeros((1, neg_length))), axis=1).T
 
-X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(database, Y, test_size=0.2,
+X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(dataset, Y, test_size=0.2,
                                                                      random_state=0)
 nb_classes = 2
 # convert class vectors to binary class matrices
@@ -28,13 +40,15 @@ Y_train = np_utils.to_categorical(Y_train, nb_classes)
 Y_test = np_utils.to_categorical(Y_test, nb_classes)
 
 print(X_train.shape)
+print(Y_train.shape)
 print(len(Y_test))
 
 maxlen = max_len
 size = vec_dim
-X_train = X_train.reshape(X_train.shape[0], 1, maxlen, size)
-X_test = X_test.reshape(X_test.shape[0], 1, maxlen, size)
 print(X_train.shape)
+
+# Number of feature maps (outputs of convolutional layer)
+N_fm = 300
 
 batch_size = 128
 nb_epoch = 1
@@ -66,17 +80,35 @@ model.compile(loss='categorical_crossentropy', optimizer='adadelta')
 ####################################################################################
 
 
+epoch = 0
+val_acc = []
+val_auc = []
 
+# Train model for N_epoch epochs (could be run as many times as needed)
 
+N_epoch = 3
 
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, validation_data=(X_test, Y_test))
+for i in range(N_epoch):
+    model.fit(X_train, Y_train, batch_size=50, nb_epoch=1, verbose=1, show_accuracy=True)
+    output = model.predict_proba(X_test, batch_size=10, verbose=1)
+    # find validation accuracy using the best threshold value t
+    vacc = np.max(
+        [np.sum((output[:, 1] > t) == (Y_test[:, 1] > 0.5)) * 1.0 / len(output) for t in np.arange(0.0, 1.0, 0.01)])
+    # find validation AUC
+    vauc = roc_auc_score(Y_test, output[:, 0])
+    val_acc.append(vacc)
+    val_auc.append(vauc)
+    print('Epoch {}: validation accuracy = {:.3%}, validation AUC = {:.3%}'.format(epoch, vacc, vauc))
+    epoch += 1
 
-score = model.evaluate(X_test, Y_test)
-print('The score:', score)
-exit()
-predict = model.predict(X_test, batch_size=batch_size).reshape((1, len(Y_test)))[0]
+print('{} epochs passed'.format(epoch))
+print('Accuracy on validation dataset:')
+print(val_acc)
+print('AUC on validation dataset:')
+print(val_auc)
 
-pickle.dump((Y_test, predict), open(os.path.join('.', 'data', 'tmp', 'NN_output_CVAT.p'), "wb"))
+# Save model
+model.save_weights('cnn_3epochs.model')
 
 '''
 from evaluate import evaluate
