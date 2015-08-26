60b8d36a99a319be2f7eeecd67ccf37f31157f75
candlewill <yunchaohe@gmail.com>
2015-08-26 20:27:04 +0800

    movie_reviews

--- cnn_evaluate.py
+++ cnn_evaluate.py
@@ -3,10 +3,10 @@ __author__ = 'NLP-PC'
 from load_data import load_pickle
 from file_name import get_file_path
 
-(Y_test, predict) = load_pickle('./data/corpus/vader/cnn_all_result.p')
+(Y_test, predict) = load_pickle('./data/corpus/vader/cnn_tweets_result.p')
 
 from evaluate import evaluate
 
 print(Y_test)
 print(predict)
-evaluate(Y_test, predict, 'all')
+evaluate(Y_test, predict, 'tweets')
