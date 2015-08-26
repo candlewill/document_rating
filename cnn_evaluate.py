5d41c9a95b22922b74407b707dd821b97d4388dd
candlewill <yunchaohe@gmail.com>
2015-08-26 20:55:46 +0800

    finish experiments

--- cnn_evaluate.py
+++ cnn_evaluate.py
@@ -2,11 +2,29 @@ __author__ = 'NLP-PC'
 
 from load_data import load_pickle
 from file_name import get_file_path
-
-(Y_test, predict) = load_pickle('./data/corpus/vader/cnn_tweets_result.p')
-
 from evaluate import evaluate
 
+(Y_test, predict) = load_pickle('./data/corpus/vader/cnn_movie_news_articles.p')
 print(Y_test)
 print(predict)
-evaluate(Y_test, predict, 'tweets')
+evaluate(Y_test, predict, 'news_articles')
+
+(Y_test, predict) = load_pickle('./data/corpus/vader/cnn_movie_news_articles1.p')
+print(Y_test)
+print(predict)
+evaluate(Y_test, predict, 'news_articles')
+
+(Y_test, predict) = load_pickle('./data/corpus/vader/cnn_movie_news_articles2.p')
+print(Y_test)
+print(predict)
+evaluate(Y_test, predict, 'news_articles')
+
+(Y_test, predict) = load_pickle('./data/corpus/vader/cnn_movie_news_articles3.p')
+print(Y_test)
+print(predict)
+evaluate(Y_test, predict, 'news_articles')
+
+(Y_test, predict) = load_pickle('./data/corpus/vader/cnn_movie_news_articles4.p')
+print(Y_test)
+print(predict)
+evaluate(Y_test, predict, 'news_articles')
